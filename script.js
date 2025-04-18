9894a424a75f059a3e4bad91fc87ef65a03b586e
Carlo Vella <carlo.vella.23@um.edu.mt>
2025-04-18 21:55:03 +0200

    Added automatic resource generation via hiring Ninjas + Ghosted button

--- script.js
+++ script.js
@@ -1,16 +1,79 @@
 let shadows = 0;
 let shadowsPerClick = 1;
+let shadowsPerSec = 0;
 
 const gainShadowButton = document.getElementById('gainShadow');
 const shadowCounter = document.getElementById('shadowCounter');
+const shadowsPerSecDisplay = document.getElementById('shadowsPerSec');
+const ninjaHireButtons = document.querySelectorAll('.ninja_button');
 
-function updateDisplay(){
-    shadowCounter.textContent = shadows;
+// Ninja Object with different properties
+const ninjas = {
+    novice: { baseCost: 10, cost: 10, increment: 5, sps: 1, count: 0},
+    rogue: { baseCost: 100, cost: 100, increment:50, sps: 5, count: 0},
+    elite: { baseCost: 1000, cost: 1000, increment:500, sps: 20, count: 0},
+    grandmaster: { baseCost: 10000, cost: 10000, increment:5000, sps: 100, count: 0},
 }
+
+// Update Display
+function updateDisplay(){
+    shadowCounter.textContent = Math.round(shadows);
+    shadowsPerSecDisplay.textContent = shadowsPerSec;
+
+    // Updating button cost and count display
+    ninjaHireButtons.forEach(button => {
+        const type = button.dataset.type;
+        const ninja = ninjas[type];
+        const cost = button.querySelector('.cost');
+        const amount = button.parentElement.querySelector('.count');
+
+        cost.textContent = ninja.cost;
+        amount.textContent = ninja.count;
+
+        // If not affordable, Ghost Button
+        if(shadows >= ninja.cost){
+            button.classList.remove('ghosted');
+            button.disabled = false;
+        } else {
+            button.classList.add('ghosted');
+            button.disabled = true;
+        }
+    })
+}
+
+// Gain Shadows button
 gainShadowButton.addEventListener('click', function(){
     shadows += shadowsPerClick;
     updateDisplay();
 })
 updateDisplay();
 
+// Passive Shadow gain
+setInterval(() => {
+    shadows += shadowsPerSec / 10;
+    updateDisplay();
+}, 100);
 
+// Update Ninja Statistics
+function updateNinjaStats(){
+    shadowsPerSec = 0;
+    Object.values(ninjas).forEach(ninja => {
+        shadowsPerSec += ninja.count * ninja.sps;
+    });
+    updateDisplay();
+}
+
+// Ninja Hiring
+ninjaHireButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const type = button.dataset.type;
+        const ninja = ninjas[type];
+        if (shadows >= ninja.cost){
+            shadows -= ninja.cost;
+            ninja.count ++;
+            ninja.cost = ninja.baseCost + (ninja.increment * ninja.count);
+            updateNinjaStats();
+        }
+    });
+})
+updateNinjaStats();
