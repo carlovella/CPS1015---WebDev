13be7dc526b1033ef7c019fe846e8f9817d67bdc
Carlo Vella <carlo.vella.23@um.edu.mt>
2025-04-18 13:42:48 +0200

    Added functional 'Gain Shadow' button using js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Happy developing ✨')
+let shadows = 0;
+let shadowsPerClick = 1;
+
+const gainShadowButton = document.getElementById('gainShadow');
+const shadowCounter = document.getElementById('shadowCounter');
+
+function updateDisplay(){
+    shadowCounter.textContent = shadows;
+}
+gainShadowButton.addEventListener('click', function(){
+    shadows += shadowsPerClick;
+    updateDisplay();
+})
+updateDisplay();
+
+
